7179bb9733921ec693b20cfee9374f34b1afe9d5
Dmytro <dima220v@gmail.com>
2020-04-22 19:50:56 -0700

    NotEnoughTEsts

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -20,3 +20,38 @@ TEST(PracticeTest, is_simple_palindrome)
     bool actual = obj.isPalindrome("aa");
     ASSERT_TRUE(actual);
 }
+
+TEST(PracticeTest, is_complex_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("aAcllallcAa");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, is_complex_falty_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("aAcllcallcAa");
+    ASSERT_FALSE(actual);
+}
+
+TEST(PracticeTest, is_special_char_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome(" a ");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, is_more_special_char_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome(" ++a-&-a++ ");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, is_more_special_char_falty_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome(" ++a-0&-a++ ");
+    ASSERT_FALSE(actual);
+}
